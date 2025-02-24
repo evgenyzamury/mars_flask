ad35232572b7bc7031e00650cb035377d111d365
BERSEK <zamurijevgenyij@gamil.com>
2025-02-24 17:13:12 +0300

    home 2

--- main.py
+++ main.py
@@ -276,5 +276,28 @@ def planet(planet_name: str):
         """
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname: str, level: int, rating: float):
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                  </head>
+                  <body>
+                    <h2>Результаты отбора</h2>
+                    <h3>Претендента на участие в миссии {nickname}</h3>
+                    <div class="alert-success"><br><h3>Поздравляем! Ваш рейтинг после {level} этапа отбора</h3></div>
+                    <h3><br>составляет {rating}!</h3>
+                    <div class="alert-warning"><br><h3>Желаем удачи!</h3></div>
+                  </body>
+                </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
