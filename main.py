6cd1bd62bfe9422f00a635003adca30257ff4fef
BERSEK <zamurijevgenyij@gamil.com>
2025-02-24 18:29:59 +0300

    dop 1

--- main.py
+++ main.py
@@ -299,5 +299,42 @@ def results(nickname: str, level: int, rating: float):
                 </html>"""
 
 
+@app.route('/load_photo', methods=['POST', 'GET'])
+def load_photo():
+    if request.method == 'POST':
+        with open('static/img/user.png', 'wb') as file:
+            file.write(request.files['file'].read())
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                  </head>
+                  <body>
+                    <h2 align='center'>Загрузка фотографии</h2>
+                    <h3 align='center'>для участия в миссии</h3>
+                    <div>
+                        <form method="post" enctype="multipart/form-data" class="login_form">
+                            <div class="form-group">
+                                <label for="photo">Приложите фотографию</label>
+                                <input type="file" class="form-control-file" id="photo" name="file">
+                            </div>
+                            <br>
+                            <img src={url_for('static', filename='img/user.png')}
+                            alt="Ошибка отображения изображения">
+                            <br>
+                            <br>
+                            <button type="submit" class="btn btn-primary">Отправить</button>
+                        </form>
+                    </div>
+                  </body>
+                </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
